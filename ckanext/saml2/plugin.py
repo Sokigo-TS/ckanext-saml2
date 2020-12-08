1e370a79da54cd6676b0c8bd91212cd37f992591
Joel Grafström <joel.grafstrom@sokigo.com>
2020-12-08 14:34:00 +0100

    Added possibility to redirect to same page after login. Set parameter redirect_after_login=HTTP_REFERER

--- ckanext/saml2/plugin.py
+++ ckanext/saml2/plugin.py
@@ -385,12 +385,12 @@ class Saml2Plugin(p.SingletonPlugin):
         if request.method == 'POST':
             relay_state = request.POST.get('RelayState', None)
             if relay_state:
-                came_from = get_came_from(relay_state)
-                if came_from:
-                    h.redirect_to(h.url_for(came_from))
-
-            redirect_after_login = config.get('saml2.redirect_after_login', '/dashboard')
-            h.redirect_to(redirect_after_login)
+                if (relay_state == '' or relay_state == '/start' or relay_state == '%2f' or relay_state == '%2F'):
+                    h.redirect_to('/')
+                else:
+                    h.redirect_to(relay_state)
+            else:
+                h.redirect_to('/dashboard')
 
     def _create_or_update_user(self, user_name, saml_info, name_id):
         """Create or update the subject's user account and return the user
@@ -557,8 +557,16 @@ class Saml2Plugin(p.SingletonPlugin):
         We can be here either because we are requesting a login (no user)
         or we have just been logged in.
         """
+        came_from = config.get('saml2.redirect_after_login', '/dashboard')
+        referer = p.toolkit.request.environ['HTTP_REFERER']
+        if came_from == 'HTTP_REFERER' and referer and p.toolkit.request.environ['HTTP_HOST'] == urlparse.urlparse(referer).hostname:
+            came_from = urlparse.urlparse(referer).path
+        if came_from == '/' or came_from == '':
+            came_from = '/start'
+        
         c = p.toolkit.c
         if not c.user:
+            log.info('Login requested. came_from=' + came_from + ', referer=' + referer + ', HTTP_HOST=' + p.toolkit.request.environ['HTTP_HOST'])
             try:
                 if p.toolkit.request.environ['pylons.routes_dict']['action'] == 'staff_login':
                     return
@@ -571,7 +579,16 @@ class Saml2Plugin(p.SingletonPlugin):
                     if came_from:
                         c.came_from = came_from
                     return
+
+            try:
+                if came_from:
+                    p.toolkit.request.environ['PATH_INFO'] = came_from
+            except Exception:
+                pass
+            
             return base.abort(401)
+            
+        log.info('Login requested but user is already logged in. Redirecting to dashboard.')
         h.redirect_to(controller='user', action='dashboard')
 
     def logout(self):
